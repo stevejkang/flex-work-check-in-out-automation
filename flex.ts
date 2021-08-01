ca5d76296b37f7931477b0daa10f5617eff1d244
Steve Kang <iam@juneyoung.io>
2021-08-01 15:57:32 +0900

    Wrap http requests with try-catch clause
    Create custom error class `FlexError`

--- flex.ts
+++ flex.ts
@@ -31,31 +31,45 @@ class Flex {
   }
 
   private async login(): Promise<IFlexToken> {
-    const request = await axios.post('https://amen.flex.team/actions/login', {
-      email: this.username,
-      password: this.password,
-      code: '',
-    }, {
-      headers: DEFAULT_HEADER,
-    });
-
-    return {
-      accessToken: request.data.credentials.accessToken,
-      refreshToken: request.data.credentials.refreshToken,
+    try {
+      const request = await axios.post('https://amen.flex.team/actions/login', {
+        email: this.username,
+        password: this.password,
+        code: '',
+      }, {
+        headers: DEFAULT_HEADER,
+      });
+      return {
+        accessToken: request.data.credentials.accessToken,
+        refreshToken: request.data.credentials.refreshToken,
+      }
+    } catch (error) {
+      throw new FlexError('Authentication Error');
     }
   }
 
   private async userCheckInCheck(): Promise<void> {
     const userTokenInfo = await this.login();
-    const request = await axios.get('https://amen.flex.team/actions/api/v1/users/me/work-check-in-out?date=2021-07-26', { // TODO: Set to current date
-      headers: {
-        ...DEFAULT_HEADER,
-        'x-flex-aid': userTokenInfo.accessToken,
-        'x-flex-rid': userTokenInfo.refreshToken,
-      }
-    });
-    
-    this.userStatus = request.data.data.status;
+    try {
+      const request = await axios.get('https://amen.flex.team/actions/api/v1/users/me/work-check-in-out?date=2021-07-26', { // TODO: Set to current date
+        headers: {
+          ...DEFAULT_HEADER,
+          'x-flex-aid': userTokenInfo.accessToken,
+          'x-flex-rid': userTokenInfo.refreshToken,
+        }
+      });
+      
+      this.userStatus = request.data.data.status;
+    } catch (error) {
+      throw new FlexError('User Status Check Error');
+    }
+  }
+}
+
+class FlexError extends Error {
+  constructor(errorMessage: string) {
+    super(errorMessage);
+    this.name = 'FlexError';
   }
 }
 
--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 import Flex from './flex';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 (async () => {
-  const FlexInstance = await Flex.createNew('', ''); // TODO: ENV
-  const { userStatus } = FlexInstance;
-  console.log(userStatus);
+  const username = process.env.USERNAME as string;
+  const password = process.env.PASSWORD as string;
+
+  try {
+    const FlexInstance = await Flex.createNew(username, password);
+    const { userStatus } = FlexInstance;
+    console.log(userStatus);
+  } catch (error) {
+    console.log(error);
+  }
 })();
