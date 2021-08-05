d092b246f8b97918306d5eb59225845922e6911f
Steve Kang <iam@juneyoung.io>
2021-08-05 21:06:48 +0900

    Add node-scheduler and call check-in/out api

--- index.ts
+++ index.ts
@@ -1,16 +1,29 @@
 import { Flex, FlexCheckInStatus } from './flex';
 import * as dotenv from 'dotenv';
+import * as schedule from 'node-schedule';
 dotenv.config();
 
-(async () => {
-  const username = process.env.USERNAME as string;
-  const password = process.env.PASSWORD as string;
+const username = process.env.USERNAME as string;
+const password = process.env.PASSWORD as string;
 
+// CHECK_IN
+schedule.scheduleJob(process.env.CHECKIN_CRON_RULE as string, async () => {
   try {
     const FlexInstance = await Flex.createNew(username, password);
     const { userStatus } = FlexInstance;
-    console.log(userStatus);
+    if (userStatus === FlexCheckInStatus.BEFORE_CHECK_IN) await FlexInstance.checkIn();
   } catch (error) {
     console.log(error);
   }
-})();
+});
+
+// CHECK_OUT
+schedule.scheduleJob(process.env.CHECKOUT_CRON_RULE as string, async () => {
+  try {
+    const FlexInstance = await Flex.createNew(username, password);
+    const { userStatus } = FlexInstance;
+    if (userStatus === FlexCheckInStatus.WORKING) await FlexInstance.checkIn();
+  } catch (error) {
+    console.log(error);
+  }
+});
