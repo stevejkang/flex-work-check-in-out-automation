378611f737ec0fd1a2a116426b9d427b5f28377a
Steve Kang <iam@juneyoung.io>
2021-08-06 19:46:39 +0900

    Refactor test code with `before` (Fix #1)
    Reduce unnecessary api calls

--- test/health-check/login.test.ts
+++ test/health-check/login.test.ts
@@ -1,25 +1,26 @@
 import { expect } from 'chai';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const postRequest = axios.post('https://amen.flex.team/actions/login', {
-  email: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  code: '',
-}, {
-  headers: {
-    'Origin': 'https://flex.team',
-  },
-});
+let response: AxiosResponse;
 
 describe('Check login api health', () => {
+  before(async () => {
+    response = await axios.post('https://amen.flex.team/actions/login', {
+      email: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      code: '',
+    }, {
+      headers: {
+        'Origin': 'https://flex.team',
+      },
+    });
+  });
   it('should return 200', async () => {
-    const response = await postRequest;
     expect(response.status).to.equal(200);
   });
   it('should return access and refresh token', async () => {
-    const response = await postRequest;
     expect(response.data.credentials.accessToken).to.not.be.undefined;
     expect(response.data.credentials.refreshToken).to.not.be.undefined;
   });
