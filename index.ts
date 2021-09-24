b6f04b17a0c94b838888be10581d7e888913be1a
Steve Kang <iam@juneyoung.io>
2021-09-24 19:36:24 +0900

    Remove schedule feature

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import { Flex, FlexCheckInStatus } from './flex';
 import * as dotenv from 'dotenv';
-import * as schedule from 'node-schedule';
 dotenv.config();
 
 const username = process.env.USERNAME as string;
@@ -17,25 +16,3 @@ const password = process.env.PASSWORD as string;
     console.log(error);
   }
 })();
-
-// // CHECK_IN
-// schedule.scheduleJob(process.env.CHECKIN_CRON_RULE as string, async () => {
-//   try {
-//     const FlexInstance = await Flex.createNew(username, password);
-//     const { userStatus } = FlexInstance;
-//     if (userStatus === FlexCheckInStatus.BEFORE_CHECK_IN) await FlexInstance.checkIn();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// // CHECK_OUT
-// schedule.scheduleJob(process.env.CHECKOUT_CRON_RULE as string, async () => {
-//   try {
-//     const FlexInstance = await Flex.createNew(username, password);
-//     const { userStatus } = FlexInstance;
-//     if (userStatus === FlexCheckInStatus.WORKING) await FlexInstance.checkOut();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
